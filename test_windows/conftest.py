66e9797874d4bf16e8e13bbc4b2e0899ef2e9eef
bai-design <bai520sun1314>
2020-07-30 17:11:31 +0800

    second commit

--- test_windows/conftest.py
+++ test_windows/conftest.py
@@ -8,9 +8,16 @@ def open_driver():
     yield browser
     browser.quit()
 
-@pytest.fixture(params=["https://renren.com/"])
+@pytest.fixture(params=["https://renren.com/"], ids=["测试人人网"])
 def get_url(open_driver,request):
     open_driver.get(request.param)
     yield open_driver
     print(f'请求地址: {request.param}')
 
+def pytest_collection_modifyitems(session, config, items):
+    items.reverse()
+    for item in items:
+        item._nodeid = item.nodeid.encode("utf-8").decode("unicode-escape")
+        item.name = item.name.encode("utf-8").decode("unicode-escape")
+        if "default" in item.nodeid:
+            item.add_marker(pytest.mark.miss)
--- test_windows/test_link.py
+++ test_windows/test_link.py
@@ -3,7 +3,7 @@ import pytest
 class Test_browers(object):
 
     # @pytest.mark.flaky(reruns=3, reruns_delay=2)
-    @pytest.mark.xfail(reason="no test")
+    @pytest.mark.xfail(reason="no test", ids="测试人人网")
     # @pytest.mark.run(order=1)
     # @pytest.mark.dependency(name="test_default_get")
     def test_default_get(self, get_url):
@@ -11,7 +11,7 @@ class Test_browers(object):
 
     # @pytest.mark.dependency(depends=["test_default_get"])
     @pytest.mark.parametrize(argnames="get_url", argvalues=["https://www.baidu.com/", "https://www.google.cn"],
-                             indirect=True, ids=["test_baidu", "test_gogle"])
+                             indirect=True, ids=["测试百度", "测试谷歌"])
     # @pytest.mark.run(order=3)
     def test_much_get(self, get_url):
         assert get_url.current_url
